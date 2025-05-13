5969ce546c9da7322849d9dfe11168eb5012fc39
이형준 <gggudwnsgg@naver.com>
2025-05-14 00:13:06 +0900

    project init

---
+++ Course_Registration_Process/src/main/java/com/hyoungjun/Course_Registration_Process/CourseRegistrationProcessApplication.java
@@ -0,0 +1,15 @@
+package com.hyoungjun.Course_Registration_Process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegistrationProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseRegistrationProcessApplication.class, args);
+		System.out.println("hello");
+	}
+
+
+}
---
+++ Course_Registration_Process/src/test/java/com/hyoungjun/Course_Registration_Process/CourseRegistrationProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyoungjun.Course_Registration_Process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegistrationProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
