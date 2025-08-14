f30f92c180e4969615f73ee6b1337f5e8ab2a9d6
이형준 <gggudwnsgg@naver.com>
2025-08-14 15:25:56 +0900

    commit test

---
+++ src/main/java/CourseRegistrationProcess/course_registration/CourseRegistrationProcessApplication.java
@@ -0,0 +1,16 @@
+package CourseRegistrationProcess.course_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegistrationProcessApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CourseRegistrationProcessApplication.class, args);
+		System.out.println("Course Registration Process Application Started");
+
+	}
+
+}
---
+++ src/test/java/CourseRegistrationProcess/course_registration/CourseRegistrationProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package CourseRegistrationProcess.course_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegistrationProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
